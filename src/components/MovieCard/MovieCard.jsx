36724c09c8579a161c8913884eecc4909b10727f
Evgeniy <evgnec@gmail.com>
2025-07-30 00:36:53 +0300

    upd

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,18 +1,27 @@
 import { Card, Poster, Title, Score, SectionTitle, Paragraph, InfoSection, GenreTitle, GenreList  } from './MovieCard.styled.js'
 import React from 'react'
 
-const MovieCard = (movie) => {
+const MovieCard = ({movie}) => {
+  if (!movie) return null;
+
+  const {
+    poster_path,
+    original_title,
+    popularity,
+    overview,
+    genres = [],
+  } = movie;
   return (
     <Card>
    
-    <Poster src="https://image.tmdb.org/t/p/w400//epFP29rGrLPseuKxpz3mGKr23Do.jpg" alt="Movie Poster" />
+    <Poster src={`https://image.tmdb.org/t/p/w400${poster_path}`} alt="Movie Poster" />
     <InfoSection>
-    <Title>{movie.original_title}</Title>
-    <Score>{movie.popularity}</Score>
+    <Title>{original_title}</Title>
+    <Score>{popularity}</Score>
     <SectionTitle>Overview</SectionTitle>
-    <Paragraph>{movie.overview}</Paragraph>
+    <Paragraph>{overview}</Paragraph>
     <GenreTitle>Genres:</GenreTitle>
-    <GenreList>{movie.genres.map(genre => genre.name).join(', ')}</GenreList>
+    <GenreList>{genres.map(genre => genre.name).join(', ')}</GenreList>
     <Paragraph>Comedy</Paragraph>
     </InfoSection>
   </Card>
